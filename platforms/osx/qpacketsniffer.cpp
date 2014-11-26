7c300fe42621f137ad2f6ef774df1536ffe4939e
Michael Martin Moro <michael@unetresgrossebite.com>
2014-11-26 14:27:41 +0100

    implements more configuration for the sniffing socket on OSX

--- platforms/osx/qpacketsniffer.cpp
+++ platforms/osx/qpacketsniffer.cpp
@@ -29,12 +29,10 @@ using namespace std;
 
 QPacketSniffer::QPacketSniffer(const QString& interface_name, QObject* parent) : QAbstractPacketSniffer(interface_name, parent)
 {
-  struct sockaddr_ndrv sock_address;
-
   sock = ::socket(PF_NDRV, SOCK_RAW, 0);
-
   initialize_sock_address();
   bind(sock, (struct sockaddr*)&sock_address, sizeof(sock_address));
+  initialize_sock_address();
 }
 
 QPacketSniffer::~QPacketSniffer()
@@ -52,10 +50,46 @@ void QPacketSniffer::initialize_sock_address()
   sock_address.snd_family = AF_NDRV;
 }
 
+void QPacketSniffer::initialize_protocol()
+{
+  struct ndrv_protocol_desc protocol_description;
+  struct ndrv_demux_desc    demux_description[1];
+
+  protocol_description.version         = (u_int32_t)1;
+  protocol_description.protocol_family = (u_int32_t)13458;
+  protocol_description.demux_count     = 1;
+  protocol_description.demux_list      = (struct ndrv_demux_desc*)(&demux_description);
+  demux_description[0].type            = NDRV_DEMUXTYPE_ETHERTYPE;
+  demux_description[0].length          = 2;
+  demux_description[0].data.ether_type = htons(0x0f0f);
+  setsockopt(sock, SOL_NDRVPROTO, NDRV_SETDMXSPEC, (caddr_t*)&protocol_description, sizeof(protocol_description));
+}
+
 void QPacketSniffer::run()
 {
+  int n = select()
 }
 
 void QPacketSniffer::wait()
 {
+}
+
+QPacketSniffer::Poll::Poll()
+{
+  max_fd = 0;
+}
+
+void QPacketSniffer::Poll::run()
+{
+  int n = select(max_fd, &read_set, 0, 0, 0);
+
+  if (n)
+    on_event(max_fd);
+}
+
+void QPacketSniffer::Poll::watch(int fd)
+{
+  FD_SET(fd, &read_set);
+  if (fd > max_fd)
+    max_fd = fd;
 }
--- platforms/osx/qpacketsniffer.h
+++ platforms/osx/qpacketsniffer.h
@@ -2,14 +2,29 @@
 # define OSX_QPACKET_SNIFFER_H
 
 # include "qabstractpacketsniffer.h"
+# include <sys/select.h>
 # include <sys/socket.h>
 # include <net/if.h>
 # include <net/ndrv.h>
+# include <functional>
 
 class QPacketSniffer : public QAbstractPacketSniffer
 {
     Q_OBJECT
 
+    class Poll
+    {
+      Poll();
+      ~Poll();
+
+      void watch(int fd);
+      void run();
+
+      std::function<void (int fd)> on_event;
+    private:
+      fd_set read_set;
+      int    max_fd;
+    };
 public:
     QPacketSniffer(const QString& interface_name, QObject* parent);
     ~QPacketSniffer();
@@ -18,8 +33,9 @@ public:
     void wait();
 private:
     void    initialize_sock_address();
+    void    initialize_protocol();
 
-    int     sock;
+    int                  sock;
     struct sockaddr_ndrv sock_address;
 };
 
