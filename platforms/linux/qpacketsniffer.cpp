ef3a2ffb9f4aaa9a41f5a518b744da94e94ed873
Michael Martin Moro <sexymimi64@gmail.com>
2014-11-26 00:21:38 +0000

    QPacketSniffer/Linux can identify more IP protocols

--- platforms/linux/qpacketsniffer.cpp
+++ platforms/linux/qpacketsniffer.cpp
@@ -174,9 +174,22 @@ struct Packet
       case IPv4:
       case IPv6:
       case ARP:
-          return (true);
+        return true;
     }
-    return (false);
+    return false;
+  }
+
+  bool has_ip_type() const
+  {
+    switch (get_ether_type())
+    {
+    case IPv4:
+    case IPv6:
+      return true;
+    default:
+      break ;
+    }
+    return false;
   }
 
   EtherType get_ether_type(void) const
@@ -186,62 +199,92 @@ struct Packet
 
   QString get_source_ip(void) const
   {
-    char ip_string[16];
-
-    switch (get_ether_type())
+    if (has_ip_type())
     {
-    case IPv4:
-    case IPv6:
+      char ip_string[16];
+
       inet_ntop(AF_INET, &ip->saddr, ip_string, 16);
       return ip_string;
-    default:
-      break ;
     }
     return "";
   }
 
   QString get_destination_ip(void) const
   {
-    char ip_string[16];
-
-    switch (get_ether_type())
+    if (has_ip_type())
     {
-    case IPv4:
-    case IPv6:
+        char ip_string[16];
+
       inet_ntop(AF_INET, &ip->daddr, ip_string, 16);
       return ip_string;
-    default:
-      break ;
     }
     return "";
   }
 
   QString get_protocol(void) const
   {
-    switch (ip->protocol)
+    if (has_ip_type())
     {
-    case IPPROTO_TCP:
-      return "tcp";
-    case IPPROTO_UDP:
-      return "udp";
-    case IPPROTO_ICMP:
-      return "icmp";
-    case IPPROTO_IGMP:
-      return "igmp";
-    case IPPROTO_RSVP:
-      return "rsvp";
-    case IPPROTO_SCTP:
-      return "sctp";
-    case IPPROTO_DCCP:
-      return "dccp";
-    case IPPROTO_ESP:
-      return "esp";
-    case IPPROTO_AH:
-      return "ah";
-    default:
-      break ;
+      switch (ip->protocol)
+      {
+      case IPPROTO_TCP:
+        return "tcp";
+      case IPPROTO_UDP:
+        return "udp";
+      case IPPROTO_ICMP:
+        return "icmp";
+      case IPPROTO_ICMPV6:
+          return "icmpv6";
+      case IPPROTO_IGMP:
+        return "igmp";
+      case IPPROTO_RSVP:
+        return "rsvp";
+      case IPPROTO_SCTP:
+        return "sctp";
+      case IPPROTO_DCCP:
+        return "dccp";
+      case IPPROTO_EGP:
+          return "egp";
+      case IPPROTO_ESP:
+        return "esp";
+      case IPPROTO_AH:
+        return "ah";
+      case IPPROTO_COMP:
+        return "comp";
+      case IPPROTO_BEETPH:
+        return "beethph";
+      case IPPROTO_DSTOPTS:
+        return "dstopts";
+      case IPPROTO_FRAGMENT:
+        return "fragment";
+      case IPPROTO_GRE:
+        return "gre";
+      case IPPROTO_ENCAP:
+        return "encap";
+      case IPPROTO_IDP:
+        return "idp";
+      case IPPROTO_IP:
+        return "ip";
+      case IPPROTO_IPV6:
+        return "ipv6";
+      case IPPROTO_IPIP:
+        return "ipip";
+      case IPPROTO_TP:
+        return "tp";
+      case IPPROTO_RAW:
+        return "raw";
+      case 112:
+        return "vrrp";
+      case IPPROTO_PIM:
+        return "pim";
+      default:
+        break ;
+      }
+      return QString::number(ip->protocol);
     }
-    return "?";
+    else if (get_ether_type() == ARP)
+      return "arp";
+    return "";
   }
 
   char                 buffer[65535];
